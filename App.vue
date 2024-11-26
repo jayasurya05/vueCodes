2a4d30c9b9904d570d349a160a004c94c4334c7c
jayasurya05 <101082472+jayasurya05@users.noreply.github.com>
2024-11-26 12:11:26 +0530

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,30 @@
+<template>
+
+    <createCrud />
+</template>
+
+<script>
+import createCrud from './components/create.vue';
+export default {
+  name: 'App',
+  components: {
+   createCrud
+  },
+  data(){
+      return{
+       
+      }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
